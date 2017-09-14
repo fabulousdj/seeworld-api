249bdf0987255d7640620a29c9970e9a89f2739e
Jin Dai <fabulousdj@foxmail.com>
2017-09-14 18:51:25 -0400

    Created basic text-speech conversion service for testing.

--- src/main/java/com/seeworld/api/domain/service/impl/TextSpeechConversionService.java
+++ src/main/java/com/seeworld/api/domain/service/impl/TextSpeechConversionService.java
@@ -1,5 +1,10 @@
 package com.seeworld.api.domain.service.impl;
 
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechAlternative;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.Transcript;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.TextToSpeech;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.model.AudioFormat;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
@@ -8,26 +13,46 @@ import com.seeworld.api.domain.service.ITextSpeechConversionService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 @Component
 public class TextSpeechConversionService implements ITextSpeechConversionService {
 
+    @Value("${watsonCloudService.speechToText.username}")
+    private String speechToTextServiceUsername;
+    @Value("${watsonCloudService.speechToText.password}")
+    private String speechToTextServicePassword;
+
     @Value("${watsonCloudService.textToSpeech.username}")
-    private String username;
+    private String textToSpeechServiceUsername;
     @Value("${watsonCloudService.textToSpeech.password}")
-    private String password;
+    private String textToSpeechServicePassword;
 
     @Override
     public String getSpeechToTextResult() {
-        return null;
+        SpeechToText service = new SpeechToText(
+                speechToTextServiceUsername,
+                speechToTextServicePassword);
+
+        RecognizeOptions options = new RecognizeOptions.Builder().contentType("audio/wav").build();
+
+        SpeechResults results = service.recognize(new File("hello_world.wav"), options).execute();
+        System.out.println(results);
+
+        Transcript resultTranscript = results.getResults().get(0);
+        SpeechAlternative speechAlternative = resultTranscript.getAlternatives().get(0);
+        return speechAlternative.getTranscript();
     }
 
     @Override
     public String getTextToSpeechResult() {
-        TextToSpeech service = new TextToSpeech(username, password);
+        TextToSpeech service = new TextToSpeech(
+                textToSpeechServiceUsername,
+                textToSpeechServicePassword);
 
         try {
             String text = "Hello world";
